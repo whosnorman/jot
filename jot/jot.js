d1341f4da2af1188469eca10fae123c7eddfa4fb
Matt O'Hagan <mco13@my.fsu.edu>
2014-08-17 20:54:27 -0400

    changed html structure

--- jot/jot.js
+++ jot/jot.js
@@ -2,25 +2,83 @@ window.onload=function(){
 	var storage = {};
 	var stor;
 	var timer;
-	var m=document.getElementById("input");
-	var b=document.getElementById("clear");
+	var lineNum = 0;
+	var divCont = document.getElementById("input");
+	var clearBtn = document.getElementById("clear");
+
+
+	storage = {
+		"0": "test 1", 
+		"1": "test 2"
+	}
+	localStorage.setItem('list', JSON.stringify(storage));
 
 	if (localStorage && localStorage.getItem('list')) {
 		storage = JSON.parse(localStorage.getItem('list'));
 
-		var arr = m.getElementsByTagName("li");
+		var arr = divCont.getElementsByTagName("div");
 		arr[0].parentNode.removeChild(arr[0]);
-		var length = Object.keys(storage).length;
+		var length = Object.keys(storage).length; 
+
 		for(var i=0; i < length; i++) {
-			var li = document.createElement("li");
-			li.appendChild(document.createTextNode(storage[i]));
-			m.appendChild(li);
+			var div = document.createElement("div");
+			div.setAttribute("id", lineNum);
+
+			var btn = document.createElement("div");
+			btn.setAttribute("id", lineNum);
+			btn.setAttribute("class", "left");
+			btn.appendChild(document.createTextNode('||'));
+
+			var txt = document.createElement("div");
+			txt.setAttribute("id", lineNum);
+			txt.setAttribute("contentEditable", true);
+			txt.setAttribute("class", "txt");
+			txt.appendChild(document.createTextNode(storage[i]));
+
+			div.appendChild(btn);
+			div.appendChild(txt);
+			divCont.appendChild(div);
+
+			lineNum++;
+		}
+	}
+	else {
+		storage = {
+			"0": ""
 		}
 	}
 
-	m.addEventListener("keyup", function(){
+	divCont.addEventListener("keyup", function(){
+		console.log('boom');
 		resetTimer();
 	}); 
+	var btnArray = [];
+	var txtArray = [];
+
+	var divArray = divCont.getElementsByTagName("div");
+	console.log(divArray);
+	var len = divArray.length / 3;
+	for(var i = 0; i < len; i++) {
+		var current = divArray[i * 3];
+		console.log(current);
+		var curr = current.getElementsByClassName('left')[0];
+		console.log(curr);
+		btnArray[i] = curr;
+		btnArray[i].addEventListener("click", function() {
+			txtArray[i] = current.getElementsByClassName('txt')[0];
+			txtArray[i].style.color = "#808080";
+		});
+	}
+
+
+/*
+	addEventLIstener('keypress', function(e){
+		var key = e.which || e.keyCode;
+		if (key == 13) {
+			// do stuff
+		}
+	});
+*/
 
 	// clear & reset storage set timer
 	function resetTimer(){
@@ -30,7 +88,7 @@ window.onload=function(){
 
 	// loop through each line and add it to the storage
 	function setStorage(){
-		var array = m.getElementsByTagName("li");
+		var array = divCont.getElementsByTagName("div");
 		storage = {};
 		for(var i=0, j=0; i < array.length; i++) {
 			var text = array[i].innerText;
@@ -48,15 +106,15 @@ window.onload=function(){
 	document.body.style.backgroundImage = "url('bg/"+n+".jpg')";
 
 	// clear button
-	b.addEventListener("click", function(){
-		var arr = m.getElementsByTagName("li");
+	clearBtn.addEventListener("click", function(){
+		var arr = divCont.getElementsByTagName("div");
 		for(var lcv=arr.length - 1; lcv >= 0; lcv--){
 			if(lcv===0)
 				arr[lcv].innerHTML='';
 			else
 				arr[lcv].parentNode.removeChild(arr[lcv]);
 		}
-		m.focus();
+		divCont.focus();
 		localStorage.removeItem('list');
 	});
 
